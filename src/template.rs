de5b4d8c3980b491030e191f37a5b0c865ec8092
Takeru Ohta <phjgt308@gmail.com>
2026-01-01 21:32:26 +0900

    feat: implement TryFrom for ValueTemplate from RawJsonValue

--- src/template.rs
+++ src/template.rs
@@ -6,6 +6,26 @@ pub enum ValueTemplate<'text, 'raw> {
     Generator(ValueGenerator<'text, 'raw>),
 }
 
+impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for ValueTemplate<'text, 'raw> {
+    type Error = nojson::JsonParseError;
+
+    fn try_from(raw: nojson::RawJsonValue<'text, 'raw>) -> Result<Self, Self::Error> {
+        match raw.kind() {
+            nojson::JsonValueKind::Null
+            | nojson::JsonValueKind::Boolean
+            | nojson::JsonValueKind::Integer
+            | nojson::JsonValueKind::Float
+            | nojson::JsonValueKind::String => Ok(Self::Literal(raw)),
+            nojson::JsonValueKind::Array => raw.try_into().map(Self::Array),
+            nojson::JsonValueKind::Object => raw
+                .to_object()?
+                .map(|(k, v)| Ok((k.try_into()?, v.try_into()?)))
+                .collect::<Result<_, _>>()
+                .map(Self::Object),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum ValueGenerator<'text, 'raw> {
     Integer {
