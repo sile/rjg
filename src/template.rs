864c5d9b9e5b6bb8bf8412d5fedab3d4ccb40fbc
Takeru Ohta <phjgt308@gmail.com>
2026-01-01 21:21:50 +0900

    feat: add ValueGenerator enum for template value generation

--- src/template.rs
+++ src/template.rs
@@ -3,5 +3,22 @@ pub enum ValueTemplate<'text, 'raw> {
     Literal(nojson::RawJsonValue<'text, 'raw>),
     Array(Vec<Self>),
     Object(Vec<(String, Self)>),
-    // Generator
+    Generator(ValueGenerator<'text, 'raw>),
+}
+
+#[derive(Debug)]
+pub enum ValueGenerator<'text, 'raw> {
+    Integer {
+        bits: usize,
+        signed: bool,
+    },
+    Float {
+        bits: usize,
+    },
+    String {
+        chars: usize,
+    },
+    Oneof {
+        choices: Vec<ValueTemplate<'text, 'raw>>,
+    },
 }
